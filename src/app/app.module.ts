c78b5b3e4b9d10a9889b7d55592dda8d73056b51
santosh <santosh.ece06@gmail.com>
2018-11-15 13:19:17 +0530

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
+import { CdacComponent } from './cdac/cdac.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     XyzComponent,
+    CdacComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/cdac/cdac.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cdac',
+  templateUrl: './cdac.component.html',
+  styleUrls: ['./cdac.component.css']
+})
+export class CdacComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

3afadd3b0bd44ad4310ebf88ede5724aeff1d6ec
santosh <santosh.ece06@gmail.com>
2018-11-15 17:16:24 +0530

    comp 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { CdacComponent } from './cdac/cdac.component';
 import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { Comp1Component } from './comp1/comp1.component';
     XyzComponent,
     CdacComponent,
     Comp1Component,
+    Comp2Component,
+    Comp3Component,
   ],
   imports: [
     BrowserModule

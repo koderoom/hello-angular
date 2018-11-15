53cf6fab8201da496d8debece3d2207dc270d678
santosh <santosh.ece06@gmail.com>
2018-11-15 12:51:08 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [XyzComponent]
 })
 export class AppModule { }
---
+++ src/app/xyz/xyz.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.css']
+})
+export class XyzComponent {
+
+}
